57ab2b41cd3b8acc7602f896406cdfe02c03ab15
tkddls8848 <tkddls8848@gmail.com>
2026-05-17 10:42:53 +0900

    Remove adaptation safety elements

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const express = require("express");
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
-const MAX_PORT_ATTEMPTS = 20;
 const ROOT = __dirname;
 
 app.use(
@@ -23,21 +22,6 @@ app.get("/", (_req, res) => {
   res.sendFile(path.join(ROOT, "index.html"));
 });
 
-function listen(port, attemptsLeft = MAX_PORT_ATTEMPTS) {
-  const server = app.listen(port, () => {
-    console.log(`Novel IF  http://localhost:${port}`);
-  });
-
-  server.on("error", (error) => {
-    if (error.code === "EADDRINUSE" && !process.env.PORT && attemptsLeft > 0) {
-      console.warn(`Port ${port} is in use. Trying ${port + 1}...`);
-      listen(port + 1, attemptsLeft - 1);
-      return;
-    }
-
-    console.error(error.message);
-    process.exit(1);
-  });
-}
-
-listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Novel IF  http://localhost:${PORT}`);
+});
